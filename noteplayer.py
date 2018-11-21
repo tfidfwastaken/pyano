6dcd9e645c99435b3e4d547b932aa616c407f9e8
Atharva Raykar <connectatharvaraykar@gmail.com>
2018-11-21 15:01:52 +0530

    Add delay for notes

--- noteplayer.py
+++ noteplayer.py
@@ -1,12 +1,16 @@
 # Plays dem notes
 import simpleaudio as sa
+import time
 
 def play_notes(notes):
     note_list = notes.split()
     for note in note_list:
-        wav_ob = sa.WaveObject.from_wave_file(f'notes/{note}1.wav')
-        play_ob = wav_ob.play()
-        play_ob.wait_done()
+        if note == '..':
+            time.sleep(0.5)
+        else:
+            wav_ob = sa.WaveObject.from_wave_file(f'notes/{note}1.wav')
+            play_ob = wav_ob.play()
+            play_ob.wait_done()
 
 def main():
     notes = input("Enter note sequence: ")
