49ae2b2777dd8a9d1f3657152344f8d2c6bfd7ba
siddhantrao23 <44816445+siddhantrao23@users.noreply.github.com>
2018-11-21 23:18:07 +0530

    Add files via upload

---
+++ Final/README.md
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+# Time-Tablify
+
+Requirements:
+Creates Time Tables for a primary school.
+	- Class Timetable
+	- Teacher Timetable
+	- Principal Timetable
+
+Assumptions:
+	- Working days = Monday to Friday, No working Saturday
+	- No. of periods/day = 9
+	- Each class = Section A,B,C
+	- P.T periods are together
+	- Classes = 1 to 5
+	- Subjects = Maths, SST, English 1, English 2, 
+		Science, Computers, P.T., Library, II Language
+	- Teachers are allowed to teach more than one classes(not just sections, but also classes)
+	- Classes for teachers are distributed more or less equally
+	- For a particular class, each subject is taught by one techer only
+	- same set of teachers for each set of classes.
+	- Subject feed will only be 9 Characters
+=======
+# A simple, dumb piano
+
+Play notes from text files.
+>>>>>>> bb997a83458f21038ba38f9f99cb54d0df88e7b7
---
+++ Final/noteplayer.py
@@ -0,0 +1,73 @@
+# Plays dem notes
+import simpleaudio as sa
+import time
+import sys
+import re
+
+def cleanerrors(text):
+    orig = text
+    text = re.sub(r'[^a-gs.. ]|[a-g]{2,}', ' ', text)
+    text = re.sub(r'(?<=[a-g]s).', ' ', text)
+    text = re.sub(r'\bs*', ' ', text)
+    text = re.sub(r'[be]s', ' ', text)
+    if orig.split() != text.split():
+        print("Your note file had errors. pyanosense tried fixing it, but the notes might not sound like what you intend.")
+    return text
+
+
+def play_notes(notes):
+    note_list = notes.split()
+    for note in note_list:
+        if note == '..':
+            time.sleep(0.5)
+        else:
+            wav_ob = sa.WaveObject.from_wave_file(f'notes/{note}1.wav')
+            play_ob = wav_ob.play()
+            play_ob.wait_done()
+
+def edit_notes():
+    info = """
+############ MUSIC CREATION MODE ###############
+### Type out your notes that pyano will play ###
+### Symbols:                                 ###
+### as = a sharp                             ###
+### .. = 1 note delay                        ###
+### Press CTRL+D to finish editing           ###
+################################################
+"""
+    print(info)
+    text = sys.stdin.read()
+    text = cleanerrors(text)
+    resp = input("Would you like to try again? ")
+    if resp == 'y':
+        return -1
+    else:
+        return text
+        
+def store_notes(note_text):
+    filename = input("\nEnter name of file to save notes in: ")
+    filename += ".txt"
+    print(f"{filename} written")
+    with open(filename, 'w') as notefile:
+        notefile.write(note_text)
+
+def main(arg=1):
+    if arg==1:
+        filename = input("Enter Filename: ")
+        filename+=".txt"
+        with open(filename) as f:
+            notes=f.read()
+            text = cleanerrors(notes)
+            print(text)
+            play_notes(text)
+            
+    else:
+        while True:
+            notes = edit_notes()
+            if notes != -1:
+                break
+        play_notes(notes)
+        store_notes(notes)
+
+if __name__ == '__main__':
+    main()
